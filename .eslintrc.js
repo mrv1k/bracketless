33c5a3615486498a4d8163aaf2b33f5ef58601b2
Viktor Crow <vkhotimchenko@gmail.com>
2017-09-10 19:06:35 -0400

    Initialize repo

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [2, 2],
+        "linebreak-style": [2, "unix"],
+        "quotes": [2, "single"],
+        "semi": [2, "always"]
+    }
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+Bracketless
+==
+A chrome extension that collapses text between the brackets.
+
+Not available in the Chrome web store yet.
