b893905bf666d84c127020852182ea16f8fe5863
Viktor Crow <vkhotimchenko@gmail.com>
2017-09-11 18:50:55 -0400

    Implement pause/resume icons

--- core/src/bg/background.js
+++ core/src/bg/background.js
@@ -6,9 +6,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
   if (!attachedTabs[tabId]) {
     attachedTabs[tabId] = 'collapsed';
+    chrome.browserAction.setIcon({tabId: tabId, path:'icons/pause.png'});
+    chrome.browserAction.setTitle({tabId: tabId, title:'Pause collapsing'});
+
     chrome.tabs.executeScript({file: 'src/bg/collapse.js'});
   } else if (attachedTabs[tabId]) {
     delete attachedTabs[tabId];
+    chrome.browserAction.setIcon({tabId: tabId, path:'icons/continue.png'});
+    chrome.browserAction.setTitle({tabId: tabId, title:'Enable collapsing'});
+
     chrome.tabs.executeScript({file: 'src/bg/expand.js'});
   }
 });
