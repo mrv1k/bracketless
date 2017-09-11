d29d9a0056a638722b6928167b8f5db6ae82da67
Viktor Crow <vkhotimchenko@gmail.com>
2017-09-11 01:02:09 -0400

    Add global 'chrome'

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,8 @@ module.exports = {
         "linebreak-style": [2, "unix"],
         "quotes": [2, "single"],
         "semi": [2, "always"]
+    },
+    "globals": {
+        "chrome": false
     }
 };
