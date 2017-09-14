2b7802870cf108eb1cc368ac9bd8bd4280158a5e
Viktor Crow <vkhotimchenko@gmail.com>
2017-09-13 21:36:50 -0400

    Combine 2 similar scripts functionality into 1.
    Change execution from IIFE to onMessage.

--- core/src/bg/collapse.js
+++ core/src/bg/collapse.js
@@ -1,20 +1,13 @@
-(function collapseBrackets() {
-  const elements  = document.getElementsByTagName('*');
-  
-  for (let i = 0; i < elements.length; i++) {
-    let element = elements[i];
-    
-    for (let j = 0; j < element.childNodes.length; j++) {
-      let node = element.childNodes[j];
-  
-      if (node.nodeType === 3) {
-        let text = node.nodeValue;
-        let replaceBrackets = text.replace(/\(.*\)/g, '(collapsed)');
-  
-        if (replaceBrackets !== text) {
-          element.replaceChild(document.createTextNode(replaceBrackets), node);
-        }
-      }
-    }
-  }
-})();
+function toggleCollapsing(textcontent) {
+  const bracketless = document.querySelectorAll('bracket-less');
+  console.log(bracketless);
+  if (textcontent.collapse) {
+    console.log('collapsed');
+  } else {
+    console.log('not collapsed');
+  }  
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  sendResponse(toggleCollapsing(message));
+});
