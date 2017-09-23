426b72481185da30208d42fd75c433c404c27d17
Viktor Crow <vkhotimchenko@gmail.com>
2017-09-23 03:06:43 -0400

    Prepare for use with options and clean up
    Major:
    Change RegExp from literal to constructor for use with options.
    Add upper and lower RegExp limit variables to immitate user options.
    Generate a string and insert variables to create RegExp to match text.
    Minor:
    Rename 'regEx' variable to 'matchedTextRegex' for clarity.
    Update test cases to accomodate all of the allowed matches.
    Reorganize code for readability.
    Clean up comments for readability.
    Use eslint single line comment exceptions.

--- core/src/bg/injecthtml.js
+++ core/src/bg/injecthtml.js
@@ -1,24 +1,31 @@
-// RegExp test cases here: http://regexr.com/3go12
+// RegExp test cases here: http://regexr.com/3gqin
 
 function injectHTML() {
   // select everything in the body except <script>, <style>, <a>, <code>, <pre>
   const elements = document.querySelectorAll('body *:not(script):not(style):not(a):not(code):not(pre)');
 
+  const userLowerLimit = 10;
+  const userUpperLimit = 100;
+  // eslint-disable-next-line no-useless-escape
+  const generatedRegexTest = `(\\()([A-Z .,!?:"'\`\\\/&-]{${userLowerLimit},${userUpperLimit}})\\w*(\\))`;
+  // generatedRegexTest =  /(\()([A-Z .,!?:"'`\\\/&-]{num,num})\w*(\))/gi
+  const inBracketsRegex = new RegExp(generatedRegexTest, 'gi');
+
   for (let i = 0; i < elements.length; i += 1) {
     if (elements[i].hasChildNodes()) {
       elements[i].childNodes.forEach((el) => {
-        if (el.nodeType === Node.TEXT_NODE && el.nodeValue.match(/(\()([A-Z .,!?:"'`\\/&-]{10,100})\w*(\))/ig)) {
-
+        if (el.nodeType === Node.TEXT_NODE && el.nodeValue.match(inBracketsRegex)) {
           const text = el.nodeValue.substring(el.nodeValue.indexOf('(') + 1, el.nodeValue.indexOf(')'));
-          const regEx = new RegExp(text.replace(/[.,!?:"'`\\/&-()]/g, '\\$&')); // escape allowed characters + ()
 
-          // data-bracketless=""  some symbols break layout, replace/change RegExp allowed symbols
+          // escape allowed characters and "(", ")" to make it RegExp safe
+          const matchedTextRegex = new RegExp(text.replace(/[.,!?:"'`\\/&-()]/g, '\\$&'));
 
-          /* eslint-disable no-param-reassign */
+          // replace symbols that break html layout: data-bracketless=""this" breaks"
           const htmlSafeText = text.replace(/&/g, '&amp;').replace(/"/g, "'");
+
+          // eslint-disable-next-line no-param-reassign
           el.parentNode.innerHTML = el.parentNode.innerHTML
-            .replace(regEx, `<bracket-less data-bracketless="${htmlSafeText}">${text}</bracket-less>`);
-          /* eslint-enable no-param-reassign */
+            .replace(matchedTextRegex, `<bracket-less data-bracketless="${htmlSafeText}">${text}</bracket-less>`);
         }
       });
     }
@@ -28,7 +35,7 @@ function injectHTML() {
 }
 injectHTML();
 
-// BUGS
+// RegExp BUGS
 // ignores text: "... (i.e., $99.99), ... (plus taxes) to buy it."
 // @ https://github.com/getify/You-Dont-Know-JS/blob/master/up%20%26%20going/ch1.md#values--types
 
