547ff148dc83a8faf27a157808cb06d0b6f863b6
Viktor Crow <vkhotimchenko@gmail.com>
2017-09-24 21:15:58 -0400

    Fix issue of not working for multipe tabs
    Rename for clarity:
    scriptInjected -> injected
    attachedTabs -> activeTabs
    
    Change injected type for proper work with multiple tabs.
    Injected property is still a boolean.
    Change activeTabs[tabId] toggling to boolean for clarity.

--- core/src/bg/background.js
+++ core/src/bg/background.js
@@ -1,26 +1,26 @@
 // Global variables only exist for the life of the page, so they get reset
 // each time the page is unloaded.
-const attachedTabs = {};
-let scriptInjected = false;
+const activeTabs = {};
+const injected = {};
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener((tab) => {
   const tabId = tab.id;
-  if (!scriptInjected) {
+  if (!injected[tabId]) {
     chrome.tabs.executeScript(tabId, { file: 'src/bg/injecthtml.js' }, (response) => {
-      scriptInjected = response;
+      injected[tabId] = response;
       chrome.browserAction.setIcon({ tabId, path: 'icons/continue.png' });
       chrome.browserAction.setTitle({ tabId, title: 'Enable collapsing' });
       chrome.tabs.insertCSS(tabId, { file: 'css/toggle.css' });
       chrome.tabs.executeScript(tabId, { file: 'src/bg/toggle_collapse.js' });
     });
-  } else if (!attachedTabs[tabId]) {
-    attachedTabs[tabId] = 'collapsed';
+  } else if (!activeTabs[tabId]) {
+    activeTabs[tabId] = true;
     chrome.browserAction.setIcon({ tabId, path: 'icons/pause.png' });
     chrome.browserAction.setTitle({ tabId, title: 'Pause collapsing' });
     chrome.tabs.sendMessage(tabId, { collapse: true });
-  } else if (attachedTabs[tabId]) {
-    delete attachedTabs[tabId];
+  } else if (activeTabs[tabId]) {
+    activeTabs[tabId] = false;
     chrome.browserAction.setIcon({ tabId, path: 'icons/continue.png' });
     chrome.browserAction.setTitle({ tabId, title: 'Enable collapsing' });
     chrome.tabs.sendMessage(tabId, { collapse: false });
