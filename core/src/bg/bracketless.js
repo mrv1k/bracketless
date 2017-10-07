12cafd27fc6926fa79085802cb953e56341df737
Viktor Crow <vkhotimchenko@gmail.com>
2017-10-07 00:52:44 -0400

    Major refactoring
    Update regex:
    Add escape for '&' chracter, so it's allowed within the brackets.
    Allow single numbers.
    Update text cases to displat that.
    
    Split injectTag functionality into injectTag and getNodes
    Rename elements to parentsNodes for clarity.
    Detect if element has multiple text in brackets elements.
    
    Prepare to handle multiple elements injection.
    Currently an issue with innerHTML breaking if injected.

--- core/src/bg/bracketless.js
+++ core/src/bg/bracketless.js
@@ -1,28 +1,32 @@
-// Regex test cases here: http://regexr.com/3grj5
+// Regex test cases here: https://regexr.com/3gtlq
 function genLimitedRegex(options) {
-  const genRegexStr = `(\\()([A-Z .,!?:"'\`\\\\/&+-]{${options.lowerRegexLimit},${options.upperRegexLimit}})\\w*(\\))`;
+  const genRegexStr = `(\\()([A-Z .,!?:"'\`\\\\/\\&+-]\\d?){${options.lowerRegexLimit},${options.upperRegexLimit}}(\\))`;
   return new RegExp(genRegexStr, 'gi');
 }
 
-function injectTag(options) {
-  const elements = document.querySelectorAll('body *:not(script):not(style):not(a):not(code):not(pre)');
+function injectTag(el, text) {
+  const dataSafeText = text.replace(/&/g, '&amp;').replace(/"/g, '&quot;'); // so it doesn't break html layout
+  const textRegex = new RegExp(text.replace(/[.,!?:"'`\\/&-(+)]/g, '\\$&')); // to match exactly that text
+  const taggedText = `<bracket-less data-bracketless="${dataSafeText}">${text}</bracket-less>`;
+  // eslint-disable-next-line no-param-reassign
+  el.parentNode.innerHTML = el.parentNode.innerHTML.replace(textRegex, taggedText);
+}
+
+function getNodes(options) {
+  const parentNodes = document.querySelectorAll('body *:not(script):not(style):not(a):not(code):not(pre)');
   const inBracketsRegex = genLimitedRegex(options);
 
-  for (let i = 0; i < elements.length; i += 1) {
-    if (elements[i].hasChildNodes()) {
-      elements[i].childNodes.forEach((el) => {
+  for (let i = 0; i < parentNodes.length; i += 1) {
+    if (parentNodes[i].hasChildNodes()) {
+      parentNodes[i].childNodes.forEach((el) => {
         if (el.nodeType === Node.TEXT_NODE && inBracketsRegex.test(el.textContent)) {
-          const text = el.nodeValue.substring(el.nodeValue.indexOf('(') + 1, el.nodeValue.indexOf(')'));
-
-          // escape allowed characters and "(", ")" to make it Regex safe
-          const matchedTextRegex = new RegExp(text.replace(/[.,!?:"'`\\/&-(+)]/g, '\\$&'));
-
-          // replace symbols that break html layout: data-bracketless=""this" breaks"
-          const htmlSafeText = text.replace(/&/g, '&amp;').replace(/"/g, '&quot;');
-
-          // eslint-disable-next-line no-param-reassign
-          el.parentNode.innerHTML = el.parentNode.innerHTML
-            .replace(matchedTextRegex, `<bracket-less data-bracketless="${htmlSafeText}">${text}</bracket-less>`);
+          const textArr = el.textContent.match(inBracketsRegex);
+          if (textArr.length > 1) {
+            // TODO: handle multiple text elements in one parent element
+            injectTag(el, textArr[0]);
+          } else {
+            injectTag(el, textArr[0]);
+          }
         }
       });
     }
@@ -30,7 +34,7 @@ function injectTag(options) {
 }
 
 function getOptions() {
-  chrome.storage.sync.get(null, options => injectTag(options));
+  chrome.storage.sync.get(null, options => getNodes(options));
 }
 
 function init() {
