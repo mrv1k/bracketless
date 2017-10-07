b80443fbebc657eef796b98266c4b8f0628080b2
Viktor Crow <vkhotimchenko@gmail.com>
2017-10-07 19:56:46 -0400

    Lil refactoring and cleaning
    Reorganize functions ordering to match execution flow.
    Split getNodes functionality into getNodes and testForBrackets.

--- core/src/bg/bracketless.js
+++ core/src/bg/bracketless.js
@@ -1,9 +1,3 @@
-// Regex test cases here: https://regexr.com/3gtlq
-function genLimitedRegex(options) {
-  const genRegexStr = `(\\()([A-Z .,!?:"'\`\\\\/\\&+-]\\d?){${options.lowerRegexLimit},${options.upperRegexLimit}}(\\))`;
-  return new RegExp(genRegexStr, 'gi');
-}
-
 function injectTag(el, text) {
   const dataSafeText = text.replace(/&/g, '&amp;').replace(/"/g, '&quot;'); // so it doesn't break html layout
   const textRegex = new RegExp(text.replace(/[.,!?:"'`\\/&-(+)]/g, '\\$&')); // to match exactly that text
@@ -12,23 +6,31 @@ function injectTag(el, text) {
   el.parentNode.innerHTML = el.parentNode.innerHTML.replace(textRegex, taggedText);
 }
 
+function testForBrackets(el, regex) {
+  if (el.nodeType === Node.TEXT_NODE && regex.test(el.textContent)) {
+    const textArr = el.textContent.match(regex);
+    if (textArr.length > 1) {
+      // TODO: handle multiple text elements in one parent element
+      injectTag(el, textArr[0]);
+    } else {
+      injectTag(el, textArr[0]);
+    }
+  }
+}
+
+// Regex test cases here: https://regexr.com/3gtlq
+function genLimitedRegex(options) {
+  const genRegexStr = `(\\()([A-Z .,!?:"'\`\\\\/\\&+-]\\d?){${options.lowerRegexLimit},${options.upperRegexLimit}}(\\))`;
+  return new RegExp(genRegexStr, 'gi');
+}
+
 function getNodes(options) {
   const parentNodes = document.querySelectorAll('body *:not(script):not(style):not(a):not(code):not(pre)');
   const inBracketsRegex = genLimitedRegex(options);
 
   for (let i = 0; i < parentNodes.length; i += 1) {
     if (parentNodes[i].hasChildNodes()) {
-      parentNodes[i].childNodes.forEach((el) => {
-        if (el.nodeType === Node.TEXT_NODE && inBracketsRegex.test(el.textContent)) {
-          const textArr = el.textContent.match(inBracketsRegex);
-          if (textArr.length > 1) {
-            // TODO: handle multiple text elements in one parent element
-            injectTag(el, textArr[0]);
-          } else {
-            injectTag(el, textArr[0]);
-          }
-        }
-      });
+      parentNodes[i].childNodes.forEach(el => testForBrackets(el, inBracketsRegex));
     }
   }
 }
