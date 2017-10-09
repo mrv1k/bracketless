34a12c9402add177a08e2ef78b096c97abe6f6d9
Viktor Crow <vkhotimchenko@gmail.com>
2017-10-08 23:49:12 -0400

    Add multiple brackets injection, tweaks
    Change injectTag parameters order to make more sense.
    Inject text into el.
    Add parent parameter in testForBrackets to avoid unceserray DOM search.
    
    Rename variables to convey more information.
    Disable eslint rule for the file because it complained about
    HTML reassigns.

--- core/src/bg/bracketless.js
+++ core/src/bg/bracketless.js
@@ -1,19 +1,20 @@
-function injectTag(el, text) {
+/* eslint-disable no-param-reassign */
+function injectTag(text, el) {
   const dataSafeText = text.replace(/&/g, '&amp;').replace(/"/g, '&quot;'); // so it doesn't break html layout
-  const textRegex = new RegExp(text.replace(/[.,!?:"'`\\/&-(+)]/g, '\\$&')); // to match exactly that text
-  const taggedText = `<bracket-less data-bracketless="${dataSafeText}">${text}</bracket-less>`;
-  // eslint-disable-next-line no-param-reassign
-  el.parentNode.innerHTML = el.parentNode.innerHTML.replace(textRegex, taggedText);
+  const replaceRegex = new RegExp(text.replace(/[.,!?:"'`\\/&-(+)]/g, '\\$&')); // to match exactly that text
+  const tag = `<bracket-less data-bracketless="${dataSafeText}">${text}</bracket-less>`;
+  el.innerHTML = el.innerHTML.replace(replaceRegex, tag);
 }
 
-function testForBrackets(el, regex) {
-  if (el.nodeType === Node.TEXT_NODE && regex.test(el.textContent)) {
-    const textArr = el.textContent.match(regex);
-    if (textArr.length > 1) {
-      // TODO: handle multiple text elements in one parent element
-      injectTag(el, textArr[0]);
-    } else {
-      injectTag(el, textArr[0]);
+function testForBrackets(node, regex, parent) {
+  if (node.nodeType === Node.TEXT_NODE && regex.test(node.textContent)) {
+    const textArr = node.textContent.match(regex);
+    if (textArr.length > 1) { // multiple brackets in one node
+      const replica = parent.cloneNode(true);
+      textArr.forEach(text => injectTag(text, replica));
+      parent.outerHTML = replica.outerHTML;
+    } else { // single brackets
+      injectTag(textArr[0], parent);
     }
   }
 }
@@ -25,12 +26,14 @@ function genLimitedRegex(options) {
 }
 
 function getNodes(options) {
-  const parentNodes = document.querySelectorAll('body *:not(script):not(style):not(a):not(code):not(pre)');
+  const parents = document.querySelectorAll('body *:not(script):not(style):not(a):not(code):not(pre)');
   const inBracketsRegex = genLimitedRegex(options);
 
-  for (let i = 0; i < parentNodes.length; i += 1) {
-    if (parentNodes[i].hasChildNodes()) {
-      parentNodes[i].childNodes.forEach(el => testForBrackets(el, inBracketsRegex));
+  for (let i = 0; i < parents.length; i += 1) {
+    if (parents[i].hasChildNodes()) {
+      parents[i].childNodes.forEach((child) => {
+        testForBrackets(child, inBracketsRegex, parents[i]);
+      });
     }
   }
 }
