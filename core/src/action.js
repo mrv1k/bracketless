d244bfaae3318a407495bc2438f026edeb231201
Viktor Crow <vkhotimchenko@gmail.com>
2017-10-14 23:34:31 -0400

    Fix dbl click firing single click evt

--- core/src/action.js
+++ core/src/action.js
@@ -1,15 +1,31 @@
 const elements = document.querySelectorAll('bracket-less');
+let clicks = 0;
+let timer = null;
 
-function expandBrackets() {
-  this.innerHTML = this.dataset.bracketless;
-  this.classList.toggle('bracket-less');
+function expandBrackets(e) {
+  e.innerHTML = e.dataset.bracketless;
+  e.classList.toggle('bracket-less');
 }
-function collapseBrackets() {
-  this.innerHTML = '...';
-  this.classList.toggle('bracket-less');
+function collapseBrackets(e) {
+  e.innerHTML = '...';
+  e.classList.toggle('bracket-less');
 }
-elements.forEach(el => el.addEventListener('click', expandBrackets.bind(el)));
-elements.forEach(el => el.addEventListener('dblclick', collapseBrackets.bind(el)));
+
+function clicksHandler() {
+  clicks += 1; // count clicks
+  if (clicks === 1) {
+    timer = setTimeout(() => {
+      expandBrackets(this);
+      clicks = 0;
+    }, 322);
+  } else {
+    clearTimeout(timer); // prevent single-click action
+    collapseBrackets(this);
+    clicks = 0;
+  }
+}
+
+elements.forEach(el => el.addEventListener('click', clicksHandler.bind(el)));
 
 function toggleCollapse(state) {
   if (state.collapse) { // play -> text collapsed
