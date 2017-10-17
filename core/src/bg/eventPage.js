0b25b06c8f15f83ba39b0bdfddcd7b71b44cbaec
Viktor Crow <vkhotimchenko@gmail.com>
2017-10-16 23:58:03 -0400

    BREAKING. Start replacing global variables

--- core/src/bg/eventPage.js
+++ core/src/bg/eventPage.js
@@ -18,8 +18,41 @@ function updateContextMenus(text) {
   chrome.contextMenus.update('bracketless', { title: text });
 }
 
-const activeTabs = {};
-const loadedTabs = {};
+function getTabStates() {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(null, (states) => {
+      console.log('getTabStates');
+      console.log(states);
+      resolve(states);
+    });
+  });
+}
+
+// setState(tabId, 'loaded', true);
+// setState(tabId, 'active', state.collapse);
+function setState(tabId, state, value) {
+  console.log('setState F', tabId, state, value);
+  if (state === 'loaded') {
+    const init = {};
+    const proxy = {};
+    proxy[state] = value; // {loaded: true}
+    init[tabId] = proxy; // {451: proxy[state]} -> {451: {loaded: true}}
+    chrome.storage.local.set(init);
+    // {451: {loaded: true}}
+  } else {
+    getTabStates()
+      .then((states) => {
+        console.log(states[tabId]);
+        states[tabId][state] = value;
+        console.log(states[tabId]);
+      });
+  }
+
+  // chrome.storage.local.clear();
+}
+
+// const loadedTabs = {};
+// const activeTabs = {};
 
 function load(tabId) {
   return new Promise((resolve) => {
@@ -29,7 +62,9 @@ function load(tabId) {
       updateContextMenus('Collapse brackets');
       chrome.tabs.insertCSS(tabId, { file: 'css/action.css' });
       chrome.tabs.executeScript(tabId, { file: 'src/action.js' }, () => {
-        resolve(loadedTabs[tabId] = true);
+        setState(tabId, 'loaded', true);
+        resolve();
+        // loadedTabs[tabId] = true -> tabId: {loaded: true, active: false}
       });
     });
   });
@@ -39,21 +74,45 @@ function doAction(tabId, action) {
   const state = action === 'play' ?
     { message: 'Pause collapsing', collapse: true, icon: 'pause' } :
     { message: 'Collapse brackets', collapse: false, icon: 'play' };
-  activeTabs[tabId] = state.collapse;
   chrome.browserAction.setIcon({ tabId, path: `icons/${state.icon}.png` });
   chrome.browserAction.setTitle({ tabId, title: state.message });
-  chrome.tabs.sendMessage(tabId, { collapse: state.collapse });
+  // chrome.tabs.sendMessage(tabId, { collapse: state.collapse });
+
+  setState(tabId, 'active', state.collapse);
   updateContextMenus(state.message);
 }
 
+// getTabStates()
+//   .then((s) => {
+//     console.log(s[tabId]);
+//     console.log(s[tabId].loaded);
+//     console.log(s[tabId].active);
+//   });
+
 function listenerAction(tabId) {
-  if (!loadedTabs[tabId]) {
-    load(tabId);
-  } else if (!activeTabs[tabId]) {
-    doAction(tabId, 'play');
-  } else if (activeTabs[tabId]) {
-    doAction(tabId, 'pause');
-  }
+  load(tabId)
+    .then(() => doAction(tabId, 'play'))
+    .then(() => getTabStates())
+    .then((s) => {
+      console.log(s);
+    });
+
+  // if (!loadedTabs[tabId]) {
+  // load(tabId);
+  // } else if (!activeTabs[tabId]) {
+  // doAction(tabId, 'play');
+  // getTabStates().then((v) => { console.log('listener', v); });
+  // } else if (activeTabs[tabId]) {
+  //   doAction(tabId, 'pause');
+  // }
+
+  // if (getTabStates().then(s => !s[tabId].loaded)) {
+  // not loaded, load
+  // } else if (getTabStates().then(s => !s[tabId].active)) {
+  // loaded not active, play
+  // } else if (getTabStates().then(s => s[tabId].active)) {
+  // loaded active, pause
+  // }
 }
 
 function optionalPermsCheck() {
@@ -87,6 +146,6 @@ chrome.runtime.onInstalled.addListener(() => {
   setUpContextMenus();
   chrome.browserAction.onClicked.addListener(tab => listenerAction(tab.id));
   chrome.contextMenus.onClicked.addListener((_, tab) => listenerAction(tab.id));
-  optionalPermsCheck()
-    .then(autoAction, e => console.warn(e)); // no permission, just ignore?
+  // optionalPermsCheck()
+  //   .then(autoAction, e => console.warn(e)); // no permission, just ignore?
 });
