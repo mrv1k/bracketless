243be18bbfa08c5dfa0c6bdbda0e07e7fe3df95b
Viktor Crow <vkhotimchenko@gmail.com>
2017-10-31 23:52:18 -0400

    Organize options.js
    Move validateNumInput to bottom as it is not used any fns
    Move restoreOptions bellow permissionHandler as to use it later
    
    Move user interactions listeners above automatic
    Add double enter spacing

--- core/src/options/options.js
+++ core/src/options/options.js
@@ -8,12 +8,8 @@ const permissionStatus = document.querySelector('.permission .status');
 const saveStatus = document.querySelector('.toolbar .status');
 
 const saveBtn = document.querySelector('#save-btn');
+const resetBtn = document.querySelector('#reset-btn');
 
-function validateNumInput() {
-  if (!this.checkValidity() || (Number(lowerLimitNum.value) > Number(upperLimitNum.value))) {
-    saveBtn.setAttribute('disabled', true);
-  } else { saveBtn.removeAttribute('disabled'); }
-}
 
 function setSaveStatus(text, timeout = 2000, cb) {
   saveStatus.textContent = text;
@@ -22,6 +18,52 @@ function setSaveStatus(text, timeout = 2000, cb) {
     if (cb) cb();
   }, timeout);
 }
+
+function saveOptions() {
+  chrome.storage.sync.set({
+    lowLimit: lowerLimitNum.value,
+    upLimit: upperLimitNum.value,
+    autoLoad: autoLoadBool.checked,
+    autoPlay: autoPlayBool.checked,
+  }, setSaveStatus.bind(null, 'Options saved'));
+}
+
+
+function requestPermissions(permission) {
+  chrome.permissions.request(permission, (granted) => {
+    if (granted) {
+      permissionStatus.textContent = 'granted';
+      autoPlayNote.textContent = '';
+      autoPlayBool.parentNode.classList.remove('secondary');
+      autoPlayBool.removeAttribute('disabled');
+      setSaveStatus('Don\'t forget to save!', 7000);
+      chrome.storage.local.clear(); // clear all previously saved states
+    } else {
+      permissionStatus.textContent = 'denied by the user';
+      autoLoadBool.checked = false;
+      autoPlayBool.setAttribute('disabled', true);
+    }
+  });
+}
+function removePermission(permission) {
+  chrome.permissions.remove(permission, (removed) => {
+    if (removed) {
+      permissionStatus.textContent = 'removed';
+      setSaveStatus('Don\'t forget to save!', 7000);
+      autoPlayBool.checked = false;
+      autoPlayBool.setAttribute('disabled', true);
+    }
+  });
+}
+function permissionHandler() {
+  const tabPerm = {
+    permissions: ['tabs'],
+    origins: ['http://*/', 'https://*/'],
+  };
+  if (this.checked) requestPermissions(tabPerm);
+  else removePermission(tabPerm);
+}
+
 
 function restoreOptions() {
   chrome.storage.sync.get({
@@ -47,62 +89,25 @@ function restoreOptions() {
   });
 }
 
-function saveOptions() {
-  chrome.storage.sync.set({
-    lowLimit: lowerLimitNum.value,
-    upLimit: upperLimitNum.value,
-    autoLoad: autoLoadBool.checked,
-    autoPlay: autoPlayBool.checked,
-  }, setSaveStatus.bind(null, 'Options saved'));
-}
-
-function requestPermissions(permission) {
-  chrome.permissions.request(permission, (granted) => {
-    if (granted) {
-      permissionStatus.textContent = 'granted';
-      autoPlayNote.textContent = '';
-      autoPlayBool.parentNode.classList.remove('secondary');
-      autoPlayBool.removeAttribute('disabled');
-      setSaveStatus('Don\'t forget to save!', 7000);
-      chrome.storage.local.clear(); // clear all previously saved states
-    } else {
-      permissionStatus.textContent = 'denied by the user';
-      autoLoadBool.checked = false;
-      autoPlayBool.setAttribute('disabled', true);
-    }
-  });
-}
-
-function removePermission(permission) {
-  chrome.permissions.remove(permission, (removed) => {
-    if (removed) {
-      permissionStatus.textContent = 'removed';
-      setSaveStatus('Don\'t forget to save!', 7000);
-      autoPlayBool.checked = false;
-      autoPlayBool.setAttribute('disabled', true);
-    }
-  });
-}
-
-function permissionHandler() {
-  const tabPerm = {
-    permissions: ['tabs'],
-    origins: ['http://*/', 'https://*/'],
-  };
-  if (this.checked) requestPermissions(tabPerm);
-  else removePermission(tabPerm);
-}
-
-
 function resetOptions() {
   permissionHandler(); // remove permission if any
   chrome.storage.sync.clear(setSaveStatus.bind(null, 'Options reset', 2000, restoreOptions));
 }
 
+
+function validateNumInput() {
+  if (!this.checkValidity() || (Number(lowerLimitNum.value) > Number(upperLimitNum.value))) {
+    saveBtn.setAttribute('disabled', true);
+  } else { saveBtn.removeAttribute('disabled'); }
+}
+
+
+// user interactions
+saveBtn.addEventListener('click', saveOptions);
+resetBtn.addEventListener('click', resetOptions);
+autoLoadBool.addEventListener('change', permissionHandler);
+
+// invoked automatically
 lowerLimitNum.addEventListener('input', validateNumInput);
 upperLimitNum.addEventListener('input', validateNumInput);
 document.addEventListener('DOMContentLoaded', restoreOptions);
-
-saveBtn.addEventListener('click', saveOptions);
-document.querySelector('#reset-btn').addEventListener('click', resetOptions);
-autoLoadBool.addEventListener('change', permissionHandler);
